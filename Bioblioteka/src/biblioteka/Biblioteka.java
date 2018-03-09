e4ba516eaa5ed8e034b9793d3173ba5f753b5e41
VanjaVlahovic <vlahovicvanja@gmail.com>
2018-03-09 15:05:38 +0100

    Revert "Moj javni komentar"
    This reverts commit 86225f51d75ec96050ea595a24f9a9cc291df8e3.

--- Bioblioteka/src/biblioteka/Biblioteka.java
+++ Bioblioteka/src/biblioteka/Biblioteka.java
@@ -31,7 +31,6 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
-		// moj javni komentar
 		if (naslov==null)
 			throw new RuntimeException("Morate uneti naslov");
 		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
